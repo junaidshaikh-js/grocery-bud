2924a5785bb0f71cc0867d1299adf3363f5a4a9c
Junaid <sj.shaikhjunaid@gmail.com>
2021-10-29 21:34:31 +0530

    add delete and editing feature in list

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 const groceryForm = document.querySelector(".grocery-form");
 const groceryInput = document.querySelector("input");
+const submitBtn = document.querySelector(".submit-btn");
 const groceryContainer = document.querySelector(".grocery-container");
 const groceryList = document.querySelector(".grocery-list");
 const alertMessage = document.querySelector(".alert");
+const listClearBtn = document.querySelector(".clear-btn");
 
+let editElement;
+let editflag = false;
+
+// add item to the list
 function createItem(title) {
   const article = document.createElement("article");
   article.classList.add("grocery-item");
@@ -34,22 +40,79 @@ function showAlert(text, action) {
 function addItem(e) {
   e.preventDefault();
 
+  if (editflag) {
+    const editedValue = groceryInput.value;
+    editElement.children[0].innerHTML = editedValue;
+
+    submitBtn.innerHTML = "submit";
+    groceryInput.value = "";
+    editflag = false;
+
+    showAlert("List updated", "success");
+    return;
+  }
+
   const groceryInputValue = groceryInput.value;
 
   if (!groceryInputValue) {
     showAlert("Enter Some Value", "danger");
+    return;
   } else {
-    if (groceryList.children.length >= 0) {
-      groceryContainer.classList.add("show-container");
-    }
-
-    const newItem = createItem(groceryInputValue);
-
-    groceryList.append(newItem);
-
-    groceryInput.value = "";
-    groceryInput.focus();
+    showAlert("Item added to the list", "success");
   }
+
+  groceryContainer.classList.add("show-container");
+
+  const newItem = createItem(groceryInputValue);
+
+  groceryList.append(newItem);
+
+  groceryInput.value = "";
 }
 
 groceryForm.addEventListener("submit", addItem);
+
+// remove all items from the list
+
+listClearBtn.addEventListener("click", clearList);
+
+function clearList() {
+  groceryList.innerHTML = "";
+
+  groceryContainer.classList.remove("show-container");
+
+  showAlert("List is Empty", "danger");
+}
+
+// delete or edit item in the list
+// use event delegation to access the edit and delete button
+
+groceryList.addEventListener("click", updateList);
+
+function updateList(e) {
+  if (e.target.parentElement.tagName == "BUTTON") {
+    const currentBtn = e.target.parentElement;
+    const currentItem = currentBtn.parentElement.parentElement;
+
+    if (currentBtn.classList.contains("delete-btn")) {
+      groceryList.removeChild(currentItem);
+
+      if (groceryList.children.length == 0) {
+        groceryContainer.classList.remove("show-container");
+      }
+
+      showAlert("item removed", "danger");
+    }
+
+    if (currentBtn.classList.contains("edit-btn")) {
+      let currentValue = currentItem.childNodes[0].textContent;
+
+      groceryInput.value = currentValue;
+
+      submitBtn.innerHTML = "edit";
+
+      editElement = currentItem;
+      editflag = true;
+    }
+  }
+}
